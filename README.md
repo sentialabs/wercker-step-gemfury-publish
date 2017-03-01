ffe25a91aff871231c4dc47ba73fa48b441ac094
Thomas A. de Ruiter <thomas.de.ruiter@sentia.com>
2017-03-01 11:24:26 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# sentialabs/gemfury-publish
+
+Builds a gem and publishes to Gemfury
+
+```yaml
+deploy:
+  steps:
+    - sentialabs/gemfury-publish:
+        gemspec: mygem.gemspec
+        org: myfuryorg
+        token: mYfUrYtOkEn-S
+```
---
+++ run.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+main() {
+  # Validate Input
+  if [ -z "$WERCKER_GEMFURY_PUBLISH_ORG" ]; then
+    fail "org: argument cannot be empty"
+  fi
+
+  if [ -z "$WERCKER_GEMFURY_PUBLISH_GEMSPEC" ]; then
+    fail "gemspec: argument cannot be empty"
+  fi
+
+  if [ ! -f "$WERCKER_GEMFURY_PUBLISH_GEMSPEC" ]; then
+    fail "gemspec: file not found '$WERCKER_GEMFURY_PUBLISH_GEMSPEC'"
+  fi
+
+
+  # Fallback to global FURY_AUTH variable
+  if [ -z "$WERCKER_GEMFURY_PUBLISH_TOKEN" ]; then
+    if [ -z "$WERCKER_GEMFURY_PUBLISH_TOKEN" ]; then
+      fail "token: argument emtpy and no global \$FURY_AUTH set"
+    fi
+    WERCKER_GEMFURY_PUBLISH_TOKEN=$FURY_AUTH
+  fi
+
+
+  # Discover gem name and version from the gemspec
+  WERCKER_GEMFURY_PUBLISH_GEM_NAME=`ruby -r rubygems -e "puts Gem::Specification::load('$WERCKER_GIT_REPOSITORY.gemspec').name"`
+  WERCKER_GEMFURY_PUBLISH_GEM_VERSION=`ruby -r rubygems -e "puts Gem::Specification::load('$WERCKER_GIT_REPOSITORY.gemspec').version"`
+  WERCKER_GEMFURY_PUBLISH_GEM="$WERCKER_GEMFURY_PUBLISH_GEM_NAME-$WERCKER_GEMFURY_PUBLISH_GEM_VERSION.gem"
+
+  if [ -z "$WERCKER_GEMFURY_PUBLISH_GEM_NAME" ]; then
+    fail "could not determine name from the gemspec"
+  fi
+
+  if [ -z "$WERCKER_GEMFURY_PUBLISH_GEM_VERSION" ]; then
+    fail "could not determine version from the gemspec"
+  fi
+
+
+  # Build the gem
+  gem build $WERCKER_GEMFURY_PUBLISH_GEMSPEC
+  if [ ! -f "$WERCKER_GEMFURY_PUBLISH_GEM" ]; then
+    fail "failed to build '$WERCKER_GEMFURY_PUBLISH_GEM'"
+  fi
+
+
+  # Publish the gem to Gemfury
+  curl \
+    -F "package=@$WERCKER_GEMFURY_PUBLISH_GEM" \
+    https://$WERCKER_GEMFURY_PUBLISH_TOKEN@push.fury.io/$WERCKER_GEMFURY_PUBLISH_ORG/
+}
+
+main
